e25ebb901419e49d83e8be5b026cf66b8e46091c
Prince Carl Ajoc <chingace471@gmail.com>
2024-10-20 12:37:09 +0800

    codelaborate commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,16 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/f4d60c73af.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="{{ asset('resources/css/home.css') }}">
     <title>Document</title>
 </head>
 <body>
-    <h1>{{ $name }}</h1>
+    <div class="d-flex">
+        <div>
+            <!-- left nav -->
+        </div>
+        <div>
+            <!-- top nav -->
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
+
+<!--  <h1>{{ $name }}</h1>
     <img src="{{ asset('storage/' . $profile_picture) }}" alt="Profile" style="height: 100px; width:100px;">
     <form action="{{ route('logout') }}" method="POST">
         @csrf
         <button type="submit" class="btn">
             Logout
         </button>
-    </form>
-</body>
-</html>
+    </form> -->
